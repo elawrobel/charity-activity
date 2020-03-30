2e849570a5c91de76fb7dee80e6ab3e76065caa0
Przemek Lach <lachprzemyslaw@pc8.home>
2020-03-30 23:47:19 +0200

    test db - to remove

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -11,3 +12,41 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+database.ref('events').push({
+  name: 'name',
+  link: 'https',
+  goal: 'goal',
+  description: 'description',
+  date: Date.now(),
+});
+
+database.ref('cities').set(['warsaw', 'cracov', 'silesia']);
+database.ref('distance').set([3, 5, 6, 10, 40]);
+database.ref('type').set(['run', 'bike', 'nordic walking']);
+
+// {
+//   "events": {
+//     name: "name",
+//     link: "http",
+//     goal: "goal",
+//     city:"warsaw,
+//     date: "2020-12-02 15:00", timestamp
+//     distance: 3,
+//     type: "run",
+//     description: "description",
+//   }
+// }
+
+// {
+//   cities: ['warsaw', 'cracov'];
+// }
+
+// {
+//   distance: [3, 5, 6, 10, 40];
+// }
+
+// {
+//   type: ['run', 'bicycle', 'nordicwalking'];
+// }
